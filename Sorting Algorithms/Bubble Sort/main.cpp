ca3ca47958cb87a5882b631810bfd2ed7bdb6240
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2021-06-08 06:06:50 +0200

    Create main.cpp

---
+++ Sorting Algorithms/Bubble Sort/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Function to print the Elements of Array
+void print_Array(int A[], int size) {
+    cout << "The Elements of the Array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+}
+
+// Function to sort using Bubble Sort Algorithm
+void bubble_sort(int A[], int size) {
+    for (int i = 0; i < (size - 1); ++i) {
+        for (int j = size; j > i; --j) {
+            if (A[j - 1] > A[j]) { // compare two adjacent elements
+                // swapping elements if elements are not in the right order
+                swap(A[j - 1], A[j]);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the Size of the Array:";
+    cin >> n;
+    const int SIZE = n;
+    int x[SIZE];
+    cout << "Enter the Elements of the Array:" << endl;
+    for (int i = 0; i < SIZE; ++i) {
+        cin >> x[i];
+    }
+    bubble_sort(x, SIZE);
+    print_Array(x, SIZE);
+    return 0;
+}
