49c7a9ce2f4b2577ed671c5ae38a1829b30ce760
NOURELDIN OSAMA <theprogrammernoureldinosama@gmail.com>
2021-06-08 02:29:57 +0200

    edit selection_sort() function

--- main.cpp
+++ main.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void selection_sort(int A[],int SIZE){
-    int index_min, temp;
-    for (int i = 0; i < SIZE-1; ++i) {
+// Function to sort using selection sort algorithm
+void selection_sort(int A[], int size) {
+    int index_min;
+    for (int i = 0; i < size - 1; ++i) {
         index_min = i;
-        for (int j = i+1; j < SIZE; ++j) {
-            if (A[j]<A[index_min]){
-                index_min=j;
+        // find minimum
+        for (int j = i + 1; j < size; ++j) {
+            if (A[j] < A[index_min]) {
+                index_min = j;
             }
         }
         // put min at the correct index
-        temp = A[index_min];
-        A[index_min]=A[i];
-        A[i] = temp;
+        swap(A[index_min], A[i]);
     }
 }
 
-int main (){
+int main() {
     int n;
-    cout<<"Enter the Size of the Array:";
-    cin>>n;
+    cout << "Enter the Size of the Array:";
+    cin >> n;
     const int SIZE = n;
     int x[SIZE];
-    cout<<"Enter the Elements of the Array:";
+    cout << "Enter the Elements of the Array:" << endl;
     for (int i = 0; i < SIZE; ++i) {
-        cin>>x[i];
+        cin >> x[i];
     }
-    selection_sort(x,SIZE);
-    cout<<"The Elements of the Array:";
+    selection_sort(x, SIZE);
+    cout << "The Elements of the Array:" << endl;
     for (int i = 0; i < SIZE; ++i) {
-        cout<<x[i]<<" ";
+        cout << x[i] << " ";
     }
-
     return 0;
 }
